b1c6d64d994969ee2de085053649768e4669b4bd
mcadecio <33119595+mcadecio@users.noreply.github.com>
2018-10-17 11:54:27 +0100

    Add files via upload

---
+++ Crypto.java
@@ -0,0 +1,9 @@
+public class Crypto {
+    public static void main(String[] args){
+
+    }
+
+    public String normalizeText(String toNormalise){
+        String toReturn = toNormalise.replaceAll()
+    }
+}
