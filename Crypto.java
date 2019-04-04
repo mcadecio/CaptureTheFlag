0787886e81a8558b292faf8ec02ccad0a39376b0
mcadecio <33119595+mcadecio@users.noreply.github.com>
2019-04-04 18:40:40 +0100

    Delete Crypto.java

--- Crypto.java
+++
@@ -1,9 +0,0 @@
-public class Crypto {
-    public static void main(String[] args){
-
-    }
-
-    public String normalizeText(String toNormalise){
-        String toReturn = toNormalise.replaceAll()
-    }
-}
